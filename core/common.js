4a2e30bc8ae4f8ea61700e135bcbace175be820f
Danny Lin <danny0838@gmail.com>
2016-04-04 15:31:33 +0800

    選項儲存、載入、介面，HTML 本地化，console debug, log, warn, error

--- core/common.js
+++ core/common.js
@@ -10,11 +10,97 @@ var scrapbook = {};
 
 
 /********************************************************************
+ * Options
+ *******************************************************************/
+
+scrapbook.options = {
+  "console.logLevel": 3, // 0: none, 1: error, 2: warn, 3: log, 4: debug
+};
+
+scrapbook.isOptionsSynced = false;
+
+scrapbook.setOption = function (key, value, callback) {
+  scrapbook.options[key] = value;
+  chrome.storage.sync.set({ key: value }, function () {
+    if (callback) {
+      callback({ key: value });
+    }
+  });
+};
+
+scrapbook.getOption = function (key, defaultValue) {
+  var result = scrapbook.options[key];
+  if (result === undefined) {
+    result = defaultValue;
+  }
+  return result;
+};
+
+scrapbook.saveOptions = function (callback) {
+  chrome.storage.sync.set(scrapbook.options, function () {
+    if (callback) {
+      callback(scrapbook.options);
+    }
+  });
+};
+
+scrapbook.loadOptions = function (callback) {
+  chrome.storage.sync.get(scrapbook.options, function (items) {
+    for (var i in items) {
+      scrapbook.options[i] = items[i];
+    }
+    if (callback) {
+      scrapbook.isOptionsSynced = true;
+      callback(scrapbook.options);
+    }
+  });
+};
+
+
+/********************************************************************
+ * Lang
+ *******************************************************************/
+
+scrapbook.loadLanguages = function () {
+  Array.prototype.slice.call(document.getElementsByTagName("*")).forEach(function (elem) {
+    var str = elem.textContent;
+    if (/^__MSG_(.*?)__$/.test(str)) {
+      elem.textContent = chrome.i18n.getMessage(RegExp.$1);
+    }
+  });
+};
+
+scrapbook.lang = function (key, args) {
+  return chrome.i18n.getMessage(key, args);
+};
+
+
+/********************************************************************
  * Console
  *******************************************************************/
 
+scrapbook.debug = function () {
+  if (scrapbook.getOption("console.logLevel") >= 4) {
+    Function.apply.call(console.debug, console, arguments);
+  }
+};
+
 scrapbook.log = function () {
-  Function.apply.call(console.log, console, arguments);
+  if (scrapbook.getOption("console.logLevel") >= 3) {
+    Function.apply.call(console.log, console, arguments);
+  }
+};
+
+scrapbook.warn = function () {
+  if (scrapbook.getOption("console.logLevel") >= 2) {
+    Function.apply.call(console.warn, console, arguments);
+  }
+};
+
+scrapbook.error = function () {
+  if (scrapbook.getOption("console.logLevel") >= 1) {
+    Function.apply.call(console.error, console, arguments);
+  }
 };
 
 
@@ -30,3 +116,10 @@ scrapbook.doctypeToString = function (doctype) {
   ret += ">\n";
   return ret;
 };
+
+
+/********************************************************************
+ * Initialization tasks
+ *******************************************************************/
+
+scrapbook.loadOptions();
---
+++ core/options.js
@@ -0,0 +1,105 @@
+/********************************************************************
+ *
+ * Manage options
+ *
+ *******************************************************************/
+
+var OPTION_PREFIX = "opt_";
+
+function getOptionFromDocument(id) {
+  var elem = document.getElementById(OPTION_PREFIX + id);
+  switch (elem.getAttribute("type")) {
+    case "checkbox":
+      return elem.checked;
+    default:
+      return elem.value;
+  }
+}
+
+function setOptionFromDocument(id, value) {
+  var elem = document.getElementById(OPTION_PREFIX + id);
+  switch (elem.getAttribute("type")) {
+    case "checkbox":
+      elem.checked = value;
+      break;
+    default:
+      elem.value = value;
+      break;
+  }
+}
+
+window.addEventListener("DOMContentLoaded", function (event) {
+  // load languages
+  scrapbook.loadLanguages();
+
+  // form
+  var form = document.getElementById("options");
+
+  form.addEventListener("submit", function (event) {
+    for (var id in scrapbook.options) {
+      scrapbook.options[id] = getOptionFromDocument(id);
+    }
+    scrapbook.saveOptions(function () {
+      window.close();
+    });
+    event.preventDefault();
+  });
+
+  // default options
+  for (var id in scrapbook.options) {
+    var value = scrapbook.options[id];
+
+    var p = document.createElement("p");
+    form.appendChild(p);
+
+    var label = document.createElement("label");
+    label.setAttribute("for", id);
+    label.textContent = id + ": ";
+    p.appendChild(label);
+
+    switch (typeof value) {
+      case "boolean":
+        var input = document.createElement("input");
+        input.id = OPTION_PREFIX + id;
+        input.type = "checkbox";
+        input.setAttribute("checked", value ? "true" : "false");
+        p.appendChild(input);
+        break;
+      case "number":
+        var input = document.createElement("input");
+        input.id = OPTION_PREFIX + id;
+        input.type = "number";
+        input.setAttribute("value", value);
+        p.appendChild(input);
+        break;
+      default:
+        var input = document.createElement("input");
+        input.id = OPTION_PREFIX + id;
+        input.type = "text";
+        input.setAttribute("value", value);
+        p.appendChild(input);
+        break;
+    }
+  }
+
+  // submit, reset
+  var p = document.createElement("p");
+
+  var submit = document.createElement("input");
+  submit.type = "submit";
+  p.appendChild(submit);
+
+  var reset = document.createElement("input");
+  reset.type = "reset";
+  p.appendChild(reset);
+
+  form.appendChild(p);
+
+  // load from sync
+  scrapbook.loadOptions(function (options) {
+    for (var id in options) {
+      var value = options[id];
+      setOptionFromDocument(id, value);
+    }
+  });
+});
